3fb298f77d31636383a9cb1d53c976e3b8eef39d
PrestonM <pentie@gmail.com>
2018-08-27 15:48:12 +0800

    fix:test script

--- onlinewake.sh
+++ onlinewake.sh
@@ -1,31 +1,44 @@
 #!/usr/bin/env sh
 set -o nounset
 
-SERVER_URL=http://127.0.0.1:3000/regpull
-WGET=/usr/bin/wget
-WOL=/usr/sbin/ether-wake
-WOL_IF=br0
+readonly SERVER_URL=http://127.0.0.1:3000/regpull
+readonly WGET=/usr/bin/wget
+readonly WOL=/usr/sbin/ether-wake
+readonly WOL_IF=br0
 
-MACS="192.168.123.145,9C:8E:99:E4:00:00,HTPC|,9C:8E:99:E4:00:01,NAS|,9C:8E:99:E4:00:02,DESKTOP"
+CLIENTS="192.168.100.127,EC:89:14:FF:FF:FF,HUAWEI
+192.168.100.129,FC:AA:14:AF:FF:FF,DESKTOP-TEST
+192.168.100.194,3C:2E:F9:16:FF:FF,iPhone-2"
+
+join_array()
+{
+  local STRING=""
+  local DELM=$2
+  for LINE in $1; do
+    STRING="${STRING}${LINE}${DELM}"
+  done
+  echo ${STRING%${DELM}}
+}
 
 wake_by_index()
 {
-  local IDX=$1
+  local IDX=$2
   local CNT=0
-  for ITM in $MACS; do
+  for ITM in $1; do
     if [ $IDX -eq $CNT ]; then
-      local MAC=${ITM:0:17}
-      echo "$WOL -i $WOL_IF $MAC"
+      local MAC=$(echo $ITM | cut -f 2 -d ',')
+      eval "$WOL -i $WOL_IF $MAC" || true
       break
     fi
-    let "CNT+=1"
+    CNT=$(($CNT+1))
   done
 }
 
 while true; do 
-    RET=$($WGET -q -T 300 -O - "${SERVER_URL}?r=${MACS}")
-    if echo $RET | grep -q '[0-9]\+'; then
-      wake_by_index $RET
+    STAS=$(join_array "$CLIENTS" '|')
+    RET=$($WGET -q -T 300 -O - "${SERVER_URL}?r=${STAS}")
+    if [ $? -eq 0 ] && echo $RET | grep -q '[0-9]\+'; then
+      wake_by_index "$CLIENTS" $RET
     fi
     sleep 1
 done
